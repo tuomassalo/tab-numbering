5cb71ce50234eca687ae004cfa7d71ed82ee1ef3
Austin Moore <amoore17@protonmail.com>
2020-01-16 22:28:08 -0500

    Tweaked tab removal to keep checking for the precise moment a tab is removed

--- tab-numbering.js
+++ tab-numbering.js
@@ -65,16 +65,26 @@ browser.tabs.onCreated.addListener(updateAll);
 // Must listen for tabs being attached from other windows
 browser.tabs.onAttached.addListener(updateAll);
 
+// Must listen for tabs being moved
 browser.tabs.onMoved.addListener(updateAll);
 
-// Firefox seems to do this inconsistently, thus this setTimeout kludge
-browser.tabs.onRemoved.addListener(() => {
-  updateAll();
-  setTimeout(updateAll, 100);
-  setTimeout(updateAll, 500);
-  setTimeout(updateAll, 1000);
+// Must listen for tabs being removed
+browser.tabs.onRemoved.addListener((tabId, removeInfo) => {
+   /* Check that the tab has been removed every 300ms.
+      Firefox fires onRemoved BEFORE it removes the tab */
+  const checkTabRemoval = () => {
+    browser.tabs.query({}, tabs => {
+      if (tabs.filter(tab => tab.id === tabId).length === 0)
+        updateAll();
+      else
+        setTimeout(checkTabRemoval, 300);
+    });
+  };
+
+  checkTabRemoval();
 });
 
+// Must listen for tab updates
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   update(tab);
 });
